19fe11e10bd0ed64d1f1b346e68a7d96305ab4ca
agua-chile <hunch05_ceviche@icloud.com>
2025-11-21 14:51:51 -0700

    removed unusued imports

--- qabot.py
+++ qabot.py
@@ -1,8 +1,6 @@
 # Library imports
-from ibm_watsonx_ai.foundation_models import ModelInference
 from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watsonx_ai.metanames import EmbedTextParamsMetaNames
-from ibm_watsonx_ai import Credentials
 from langchain_ibm import WatsonxLLM, WatsonxEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
